ae3e1a1f029b33a71fc9b6473fac31536552d441
zukaishi <ishizuka215@gmail.com>
2020-05-04 07:58:21 +0900

    update

--- pokemonPictureBook/index.js
+++ pokemonPictureBook/index.js
@@ -1,8 +1,9 @@
 let pokemon  = document.getElementsByClassName('name')[0]
-pokemon.innerHTML = pokemon.innerHTML +  ' (' + kanaToHira(pokemon.innerHTML) + ' )'
-
-function kanaToHira(str) {
-    return str.replace(/[\u30a1-\u30f6]/g, function(match) {
+if( pokemon ) {
+    pokemon.innerHTML = pokemon.innerHTML +  ' (' + katakanaToHiragana(pokemon.innerHTML) + ')'
+}
+function katakanaToHiragana( str ) {
+    return str.replace(/[\u30a1-\u30f6]/g, function( match ) {
         var chr = match.charCodeAt(0) - 0x60;
         return String.fromCharCode(chr);
     });
